bc6ec9e3438c34988581d411eb6f8c1537e8e99d
Dashuto22 <69114248+Dashuto22@users.noreply.github.com>
2024-10-02 15:46:23 -0400

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -4,16 +4,13 @@ import { Navigate } from "react-router-dom";
 const PrivateRoute = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(null);
 
-  // Using useEffect to check if the user is logged in when the component mounts
   useEffect(() => {
     const isLoggedIn = localStorage.getItem("isLoggedIn");
-    setIsAuthenticated(!!isLoggedIn); // !!isLoggedIn converts it to a boolean
+    setIsAuthenticated(!!isLoggedIn); 
   }, []);
 
-  // Return null while authentication status is being determined
   if (isAuthenticated === null) return null;
 
-  // Redirect to login if not authenticated, otherwise render the children
   return isAuthenticated ? children : <Navigate to="/" />;
 };
 
